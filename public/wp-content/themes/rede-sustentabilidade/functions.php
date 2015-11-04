e7bc910108e3205e37f771ed1c9fd40bd99baf12
Lucas Pirola <lucaspirola@gmail.com>
2015-11-04 18:43:46 -0200

    change way utf8 is set

--- public/wp-content/themes/rede-sustentabilidade/functions.php
+++ public/wp-content/themes/rede-sustentabilidade/functions.php
@@ -1,7 +1,13 @@
 <?php
 
 global $wpdb;
-$results = $wpdb->get_results("SET NAMES 'utf8';");
+if (!$wpdb->set_charset('utf8')) {
+    printf("Error loading character set utf8: %s\n", $wpdb->error);
+} else {
+    printf("Current character set: %s\n", $wpdb->character_set_name());
+}
+
+print_r( $mysqli->get_charset() );
 
 require_once("utilidades/ApiRede.php");
 
