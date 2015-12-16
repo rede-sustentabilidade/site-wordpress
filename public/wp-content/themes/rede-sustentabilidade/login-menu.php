c0f5bfe58c354ffd30a8a9d22ef7ae6c1f084398
Lucas Pirola <lucaspirola@gmail.com>
2015-12-16 17:30:09 -0200

    desabilita codigo que recebe login oauth2 temporariamente

--- public/wp-content/themes/rede-sustentabilidade/login-menu.php
+++ public/wp-content/themes/rede-sustentabilidade/login-menu.php
@@ -2,7 +2,7 @@
 if (is_user_logged_in()) {
     global $current_user; get_currentuserinfo();
     $ApiRede = ApiRede::getInstance();
-    $filiado = $ApiRede->filiadoFormWasFilled($current_user->ID);
+    $filiado = $ApiRede->filiadoFormWasFilled($current_user->ID); // trocar para e-mail
 ?>
 <script>API_USER_STATUS = '';</script>
 <script>WP_USER_ROLE = '<?php echo get_current_user_role(); ?>';</script>
@@ -129,3 +129,82 @@ if (is_user_logged_in()) {
 		<a href="<?php echo site_url('/'); ?>?login=comecar" class="label"><strong>login</strong></a>
     </div>
 <?php } ?>
+<?php
+/* if (isset($_GET['code'])) { */
+/*     try { */
+
+/* 	$provider = new \League\OAuth2\Client\Provider\GenericProvider([ */
+/* 		'clientId'                => OAUTH_CLIENT_ID,    // The client ID assigned to you by the provider */
+/* 		'clientSecret'            => OAUTH_CLIENT_SECRET,   // The client password assigned to you by the provider */
+/* 		'redirectUri'             => OAUTH_REDIRECT_URI, */
+/* 		'urlAuthorize'            => OAUTH_URL_AUTHORIZE, */
+/* 		'urlAccessToken'          => OAUTH_URL_ACCESS_TOKEN, */
+/* 		'urlResourceOwnerDetails' => OAUTH_URL_RESOURCE */
+/* 	]); */
+/*         // Try to get an access token using the authorization code grant. */
+/*         $accessToken = $provider->getAccessToken('authorization_code', [ */
+/*             'code' => $_GET['code'] */
+/*         ]); */
+
+/*         // We have an access token, which we may use in authenticated */
+/*         // requests against the service provider's API. */
+/*         echo $accessToken->getToken() . "\n"; */
+/*         echo $accessToken->getRefreshToken() . "\n"; */
+/*         echo $accessToken->getExpires() . "\n"; */
+/*         echo ($accessToken->hasExpired() ? 'expired' : 'not expired') . "\n"; */
+
+/*         // Using the access token, we may look up details about the */
+/*         // resource owner. */
+/*         $resourceOwner = $provider->getResourceOwner($accessToken); */
+
+/*         var_dump($resourceOwner->toArray()); */
+
+/*         // The provider provides a way to get an authenticated API request for */
+/*         // the service, using the access token; it returns an object conforming */
+/*         // to Psr\Http\Message\RequestInterface. */
+/*         $request = $provider->getAuthenticatedRequest( */
+/*             'GET', */
+/*             'http://rede.usuarios/restricted', */
+/*             $accessToken */
+/*         ); */
+
+/*     } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) { */
+/* 		// Failed to get the access token or user details. */
+/* 		var_dump($e); */
+/*         exit($e->getMessage()); */
+
+/*     } */
+
+/* } else if (isset($_GET['login'])) { */
+/* // If we don't have an authorization code then get one */
+/* //} else if (!isset($_GET['code'])) { */
+/* 	$provider = new \League\OAuth2\Client\Provider\GenericProvider([ */
+/* 		'clientId'                => OAUTH_CLIENT_ID,    // The client ID assigned to you by the provider */
+/* 		'clientSecret'            => OAUTH_CLIENT_SECRET,   // The client password assigned to you by the provider */
+/* 		'redirectUri'             => OAUTH_REDIRECT_URI, */
+/* 		'urlAuthorize'            => OAUTH_URL_AUTHORIZE, */
+/* 		'urlAccessToken'          => OAUTH_URL_ACCESS_TOKEN, */
+/* 		'urlResourceOwnerDetails' => OAUTH_URL_RESOURCE */
+/* 	]); */
+
+/*     // Fetch the authorization URL from the provider; this returns the */
+/*     // urlAuthorize option and generates and applies any necessary parameters */
+/*     // (e.g. state). */
+/*     $authorizationUrl = $provider->getAuthorizationUrl(); */
+
+/*     // Get the state generated for you and store it to the session. */
+/*     $_SESSION['oauth2state'] = $provider->getState(); */
+
+/* 	// Redirect the user to the authorization URL. */
+/*     header('Location: ' . $authorizationUrl); */
+/*     exit; */
+
+/* // Check given state against previously stored one to mitigate CSRF attack */
+/* } */
+/* /1* elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) { *1/ */
+
+/* /1*     unset($_SESSION['oauth2state']); *1/ */
+/* /1*     exit('Invalid state'); *1/ */
+
+/* /1* } *1/ */
+?>
