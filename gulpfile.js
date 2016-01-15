e2bd91d1145c8d1f7763567fe2b17e4cf72195e0
Lucas Pirola <lucaspirola@gmail.com>
2016-01-15 13:45:27 -0200

    Atualiza logo e automatiza copia de arquivos php p/ built

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,19 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     imagemin = require('gulp-imagemin'),
 		pngcrush = require('imagemin-pngcrush'),
-    base_path = 'public/wp-content/themes/rede-sustentabilidade/';
+    base_path = 'public/',
+    theme_path = base_path + 'wp-content/themes/rede-sustentabilidade/';
 
 
 gulp.task('styles', function() {
-  return gulp.src(base_path + 'assets/styles/**/*.sass')
+  return gulp.src(theme_path + 'assets/styles/**/*.sass')
     .pipe(sass())
     //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     //.pipe(gulp.dest('css'))
     //.pipe(rename({suffix: '.min'}))
     .on('error', function (err) { console.log(err.message); })
     .pipe(minifycss())
-    .pipe(gulp.dest(base_path));
+    .pipe(gulp.dest(theme_path));
 });
 
 //Task for sass using libsass through gulp-sass
@@ -28,19 +29,25 @@ gulp.task('styles', function() {
 // });
 
 gulp.task('images', function () {
-    return gulp.src(base_path + 'assets/images/**/*')
+    return gulp.src(theme_path + 'assets/images/**/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngcrush()]
         }))
-        .pipe(gulp.dest(base_path + 'assets/images/'));
+        .pipe(gulp.dest(theme_path + 'assets/images/'));
+});
+
+gulp.task('copyPhp', function() {
+    gulp.src(base_path + '**/*')
+        .pipe(gulp.dest('./public.built'));
 });
 
 gulp.task('watch', function() {
 
+  gulp.watch(base_path + '**/*', ['copyPhp']);
   // Watch the sass files
-  gulp.watch(base_path + 'assets/styles/**/*.sass', ['styles']);
+  gulp.watch(theme_path + 'assets/styles/**/*.sass', ['styles']);
 
 });
 
@@ -50,4 +57,3 @@ gulp.task('default', ['watch']); //'plugins', 'scripts',
 gulp.task('imagemin', ['images']); //'plugins', 'scripts',
 
 gulp.task('build', ['styles']);
-
