7af4870c42ecd1840a6d8a05bc6058edd04d47a9
Lucas Pirola <lucaspirola@gmail.com>
2016-02-26 16:18:56 -0300

    Troca uso do sass para stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    stylus = require('gulp-stylus'),
+    // sass = require('gulp-ruby-sass'),
 //    autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
@@ -9,16 +10,26 @@ var gulp = require('gulp'),
     theme_path = base_path + 'wp-content/themes/rede-sustentabilidade/';
 
 
-gulp.task('styles', function() {
-  return gulp.src(theme_path + 'assets/styles/**/*.sass')
-    .pipe(sass())
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    //.pipe(gulp.dest('css'))
-    //.pipe(rename({suffix: '.min'}))
-    .on('error', function (err) { console.log(err.message); })
-    .pipe(minifycss())
-    .pipe(gulp.dest(theme_path));
+// gulp.task('styles', function() {
+//   return gulp.src(theme_path + 'assets/styles/**/*.sass')
+//     .pipe(sass())
+//     //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+//     //.pipe(gulp.dest('css'))
+//     //.pipe(rename({suffix: '.min'}))
+//     .on('error', function (err) { console.log(err.message); })
+//     .pipe(minifycss())
+//     .pipe(gulp.dest(theme_path));
+// });
+
+gulp.task('styles', function () {
+    gulp.src(theme_path + 'assets/styles/**/*.styl')
+		    .pipe(stylus({
+            linenos: true,
+            compress: true
+        }))
+		    .pipe(gulp.dest(theme_path));
 });
+
 
 //Task for sass using libsass through gulp-sass
 // gulp.task('sass', function(){
@@ -27,6 +38,7 @@ gulp.task('styles', function() {
 //     .on('error', function (err) { console.log(err.message); })
 //     .pipe(gulp.dest('build/css'));
 // });
+
 
 gulp.task('images', function () {
     return gulp.src(theme_path + 'assets/images/**/*')
@@ -47,7 +59,7 @@ gulp.task('watch', function() {
 
   gulp.watch(base_path + '**/*', ['copyPhp']);
   // Watch the sass files
-  gulp.watch(theme_path + 'assets/styles/**/*.sass', ['styles']);
+  gulp.watch(theme_path + 'assets/styles/**/*.styl', ['styles']);
 
 });
 
