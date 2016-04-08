f9bf0f89bb4129804e87094ffe0460c98c91d9fe
Lucas Pirola <lucaspirola@gmail.com>
2016-04-08 12:27:54 -0300

    Adiciona post type conheca e customiza output da API

--- public/wp-content/themes/rede-sustentabilidade/functions.php
+++ public/wp-content/themes/rede-sustentabilidade/functions.php
@@ -180,6 +180,127 @@ function custom_post_type_sugestao() {
 // Hook into the 'init' action
 add_action( 'init', 'custom_post_type_sugestao', 0 );
 
+// Register Custom Post Type
+function conheca_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Conheça', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Conheça', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Conheça', 'text_domain' ),
+		'name_admin_bar'        => __( 'Conheça', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Conheça', 'text_domain' ),
+		'description'           => __( 'Tópicos da página conheça', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-welcome-view-site',
+		'show_in_admin_bar'     => false,
+		'show_in_nav_menus'     => false,
+		'can_export'            => false,
+		'has_archive'           => false,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'conheca', $args );
+
+}
+add_action( 'init', 'conheca_post_type', 0 );
+
+// Register Custom Taxonomy
+function conheca_categoria_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Categorias', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Categoria', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Categoria Conheça', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'conheca_categorias', array( 'conheca' ), $args );
+
+}
+add_action( 'init', 'conheca_categoria_taxonomy', 0 );
+
+add_action( 'rest_api_init', 'slug_register_starship' );
+function slug_register_starship() {
+    register_rest_field( 'conheca',
+        'list_categories',
+        array(
+            'get_callback'    => 'slug_get_categorias',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+    register_rest_field( 'conheca',
+        'url_featured_media',
+        array(
+            'get_callback'    => 'slug_get_imagem',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function slug_get_categorias( $object, $field_name, $request ) {
+    return get_the_terms( $object[ 'id' ], 'conheca_categorias' );
+}
+
+function slug_get_imagem( $object, $field_name, $request ) {
+    return wp_get_attachment_image_url( $object[ 'featured_media'] );
+}
+
 if (isset($_COOKIE['usuario'])) {
 	$usuario = json_decode(stripslashes($_COOKIE['usuario']));
 }
