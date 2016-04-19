d71175652afdaaf4bead90e36ed3a6f63dd7ca8c
Lucas Pirola <lucaspirola@gmail.com>
2016-04-19 09:52:15 -0300

    Troca uso do hostname para variavel de ambiente na definicao do ambiente

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+* The base configurations of the WordPress.
+*
+* This file has the following configurations: MySQL settings, Table Prefix,
+* Secret Keys, WordPress Language, and ABSPATH. You can find more information
+* by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+* wp-config.php} Codex page. You can get the MySQL settings from your web host.
+*
+* This file is used by the wp-config.php creation script during the
+* installation. You don't have to use the web site, you can just copy this file
+* to "wp-config.php" and fill in the values.
+*
+* @package WordPress
+*/
 
 define( 'WP_ALLOW_MULTISITE', true );
 
@@ -22,30 +22,35 @@ define('DISALLOW_FILE_MODS',true);
 
 // Try environment variable 'WP_ENV'
 if (getenv('WP_ENV') !== false) {
-    // Filter non-alphabetical characters for security
-    define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
+  // Filter non-alphabetical characters for security
+  define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
 }
+
+if (!defined('WP_ENV')) {
+  define('WP_ENV', 'development');
+}
+
 // Define site host
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-    $hostname = $_SERVER['HTTP_X_FORWARDED_HOST'];
+  $hostname = $_SERVER['HTTP_X_FORWARDED_HOST'];
 } else {
-    $hostname = $_SERVER['HTTP_HOST'];
+  $hostname = $_SERVER['HTTP_HOST'];
 }
 // If WordPress has been bootstrapped via WP-CLI detect environment from --env=<environment> argument
 if (PHP_SAPI == "cli" && defined('WP_CLI_ROOT')) {
-    foreach ($argv as $arg) {
-        if (preg_match('/--env=(.+)/', $arg, $m)) {
-            define('WP_ENV', $m[1]);
-        }
+  foreach ($argv as $arg) {
+    if (preg_match('/--env=(.+)/', $arg, $m)) {
+      define('WP_ENV', $m[1]);
     }
+  }
 }
 
 // Are we in SSL mode?
 if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ||
-    (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
-    $protocol = 'https://';
+(!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+  $protocol = 'https://';
 } else {
-    $protocol = 'http://';
+  $protocol = 'http://';
 }
 // Define WordPress Site URLs if not already set in config files
 // if (!defined('WP_SITEURL')) {
@@ -55,7 +60,7 @@ if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ||
 //     define('WP_HOME', $protocol . rtrim($hostname, '/'));
 // }
 // Clean up
-unset($hostname, $protocol);
+//unset($hostname, $protocol);
 
 // Set SSL'ed domain
 if ( !empty( $_ENV["SSL_DOMAIN"] ) ) {
@@ -67,8 +72,8 @@ if ( !empty( $_ENV["SSL_DOMAIN"] ) ) {
 define( 'JETPACK_SIGNATURE__HTTPS_PORT', 80 );
 
 /**#@+
- * Memcache settings.
- */
+* Memcache settings.
+*/
 if ( !empty( $_ENV["MEMCACHIER_SERVERS"] ) ) {
   $_mcsettings = parse_url($_ENV["MEMCACHIER_SERVERS"]);
 
@@ -89,10 +94,10 @@ if ( !empty( $_ENV["MEMCACHIER_SERVERS"] ) ) {
 /**#@-*/
 
 /**#@+
- * MySQL settings.
- *
- * We are getting Heroku ClearDB settings from Heroku Environment Vars
- */
+* MySQL settings.
+*
+* We are getting Heroku ClearDB settings from Heroku Environment Vars
+*/
 if ( isset( $_ENV["DATABASE_URL"] ) ) {
   $_dbsettings = parse_url($_ENV["DATABASE_URL"]);
 } else {
@@ -121,14 +126,14 @@ if ( isset( $_ENV["CLEARDB_SSL"] ) && 'ON' == $_ENV["CLEARDB_SSL"] ) {
 /**#@-*/
 
 /**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+* Authentication Unique Keys and Salts.
+*
+* Change these to different unique phrases!
+* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+*
+* @since 2.6.0
+*/
 $_saltKeys = array(
   'AUTH_KEY',
   'SECURE_AUTH_KEY',
@@ -143,10 +148,10 @@ $_saltKeys = array(
 foreach ( $_saltKeys as $_saltKey ) {
   if ( !defined( $_saltKey ) ) {
     define(
-      $_saltKey,
-      empty( $_ENV[ $_saltKey ] ) ? 'herokuwp' : $_ENV[ $_saltKey ]
-    );
-  }
+    $_saltKey,
+    empty( $_ENV[ $_saltKey ] ) ? 'herokuwp' : $_ENV[ $_saltKey ]
+  );
+}
 }
 
 unset( $_saltKeys, $_saltKey );
@@ -154,107 +159,91 @@ unset( $_saltKeys, $_saltKey );
 /**#@-*/
 
 /**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+* WordPress Database Table prefix.
+*
+* You can have multiple installations in one database if you give each a unique
+* prefix. Only numbers, letters, and underscores please!
+*/
 $table_prefix  = 'wp_';
 
 /**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
+* WordPress Localized Language, defaults to English.
+*
+* Change this to localize WordPress. A corresponding MO file for the chosen
+* language must be installed to wp-content/languages. For example, install
+* de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+* language support.
+*/
 define('WPLANG', 'pt_BR');
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
-// Try server hostname
-if (!defined('WP_ENV')) {
-    // Set environment based on hostname
-    switch ($hostname) {
-      case 'redesustentabilidade.org.br':
-      case 'www.redesustentabilidade.org.br':
-        define('WP_ENV', 'production');
-    		define('WP_CACHE', false);
-    		define('WP_API_PATH', 'https://api-v1.redesustentabilidade.org.br/');
-    		define('WP_PASSPORT_PATH',       'https://passaporte.redesustentabilidade.org.br');
-    		define('OAUTH_REDIRECT_URI',     'https://redesustentabilidade.org.br/');
-    		define('OAUTH_CLIENT_ID',        getenv('OAUTH_CLIENT_ID'));
-    		define('OAUTH_CLIENT_SECRET',    getenv('OAUTH_CLIENT_SECRET'));
-    		define('OAUTH_URL_AUTHORIZE',    'https://passaporte.redesustentabilidade.org.br/oauth/authorization');
-    		define('OAUTH_URL_ACCESS_TOKEN', 'https://passaporte.redesustentabilidade.org.br/oauth/token');
-    		define('OAUTH_URL_RESOURCE',     'https://passaporte.redesustentabilidade.org.br/oauth/resource');
+// Set environment based on hostname
+switch (WP_ENV) {
+  case 'production':
+  define('WP_DEBUG', false);
+  define('WP_CACHE', true);
+  define('WP_API_PATH', 'https://api-v1.redesustentabilidade.org.br/');
+  define('WP_PASSPORT_PATH',       'https://passaporte.redesustentabilidade.org.br');
+  define('OAUTH_REDIRECT_URI',     'https://redesustentabilidade.org.br/');
+  define('OAUTH_CLIENT_ID',        getenv('OAUTH_CLIENT_ID'));
+  define('OAUTH_CLIENT_SECRET',    getenv('OAUTH_CLIENT_SECRET'));
+  define('OAUTH_URL_AUTHORIZE',    'https://passaporte.redesustentabilidade.org.br/oauth/authorization');
+  define('OAUTH_URL_ACCESS_TOKEN', 'https://passaporte.redesustentabilidade.org.br/oauth/token');
+  define('OAUTH_URL_RESOURCE',     'https://passaporte.redesustentabilidade.org.br/oauth/resource');
 
-        define('MULTISITE', true);
-        define('SUBDOMAIN_INSTALL', false);
-        define('DOMAIN_CURRENT_SITE', 'redesustentabilidade.org.br');
-        define('PATH_CURRENT_SITE', '/');
-        define('SITE_ID_CURRENT_SITE', 1);
-        define('BLOG_ID_CURRENT_SITE', 1);
-        break;
+  // define('MULTISITE', true);
+  // define('SUBDOMAIN_INSTALL', false);
+  // define('DOMAIN_CURRENT_SITE', 'redesustentabilidade.org.br');
+  // define('PATH_CURRENT_SITE', '/');
+  // define('SITE_ID_CURRENT_SITE', 1);
+  // define('BLOG_ID_CURRENT_SITE', 1);
+  break;
 
-      case 'redesustentabilidade.net':
-	    case 'www.redesustentabilidade.net':
-    		define('WPHTTPS_RESET', true);
-        define('WP_ENV', 'staging');
-    		define('WP_CACHE', false);
-    		define('WP_API_PATH', 'http://api-v1.redesustentabilidade.net');
-    		define('WP_PASSPORT_PATH',       'http://passaporte.redesustentabilidade.net');
-    		define('OAUTH_REDIRECT_URI',     'http://redesustentabilidade.net/');
-    		define('OAUTH_CLIENT_ID',        'XnvqtV7U');
-    		define('OAUTH_CLIENT_SECRET',    '00UlvMJicqoY8y3qtFoY');
-    		define('OAUTH_URL_AUTHORIZE',    'http://passaporte.redesustentabilidade.net/oauth/authorization');
-    		define('OAUTH_URL_ACCESS_TOKEN', 'http://passaporte.redesustentabilidade.net/oauth/token');
-    		define('OAUTH_URL_RESOURCE',     'http://passaporte.redesustentabilidade.net/oauth/resource');
+  case 'staging':
+  define('WPHTTPS_RESET', true);
+  define('WP_DEBUG', true);
+  define('WP_CACHE', false);
+  define('WP_API_PATH', 'http://api-v1.redesustentabilidade.net');
+  define('WP_PASSPORT_PATH',       'http://passaporte.redesustentabilidade.net');
+  define('OAUTH_REDIRECT_URI',     'http://redesustentabilidade.net/');
+  define('OAUTH_CLIENT_ID',        'XnvqtV7U');
+  define('OAUTH_CLIENT_SECRET',    '00UlvMJicqoY8y3qtFoY');
+  define('OAUTH_URL_AUTHORIZE',    'http://passaporte.redesustentabilidade.net/oauth/authorization');
+  define('OAUTH_URL_ACCESS_TOKEN', 'http://passaporte.redesustentabilidade.net/oauth/token');
+  define('OAUTH_URL_RESOURCE',     'http://passaporte.redesustentabilidade.net/oauth/resource');
 
-        define('MULTISITE', true);
-        define('SUBDOMAIN_INSTALL', true);
-        define('DOMAIN_CURRENT_SITE', 'redesustentabilidade.net');
-        define('PATH_CURRENT_SITE', '/');
-        define('SITE_ID_CURRENT_SITE', 1);
-        define('BLOG_ID_CURRENT_SITE', 1);
+  define('MULTISITE', true);
+  define('SUBDOMAIN_INSTALL', true);
+  define('DOMAIN_CURRENT_SITE', 'redesustentabilidade.net');
+  define('PATH_CURRENT_SITE', '/');
+  define('SITE_ID_CURRENT_SITE', 1);
+  define('BLOG_ID_CURRENT_SITE', 1);
 
-        break;
-      case 'rede.site':
-      default:
-        define('WP_ENV', 'development');
-    		define('WP_CACHE', false);
-    		define('WP_API_PATH',            'http://rede.api:9000');
-    		define('WP_PASSPORT_PATH',       'http://rede.passaporte:3000');
-    		define('OAUTH_REDIRECT_URI',     'http://rede.site/');
-    		define('OAUTH_CLIENT_ID',        'LiwFKQ0b');
-    		define('OAUTH_CLIENT_SECRET',    'vEdVSdklM6Y1Vo5HFWkz');
-    		define('OAUTH_URL_AUTHORIZE',    'http://rede.passaporte:3000/oauth/authorization');
-    		define('OAUTH_URL_ACCESS_TOKEN', 'http://rede.passaporte:3000/oauth/token');
-    		define('OAUTH_URL_RESOURCE',     'http://rede.passaporte:3000/oauth/resource');
+  break;
+  default:
+  define('WP_DEBUG', true);
+  define('WP_CACHE', false);
+  define('WP_API_PATH',            'http://rede.api:9000');
+  define('WP_PASSPORT_PATH',       'http://rede.passaporte:3000');
+  define('OAUTH_REDIRECT_URI',     'http://rede.site/');
+  define('OAUTH_CLIENT_ID',        'LiwFKQ0b');
+  define('OAUTH_CLIENT_SECRET',    'vEdVSdklM6Y1Vo5HFWkz');
+  define('OAUTH_URL_AUTHORIZE',    'http://rede.passaporte:3000/oauth/authorization');
+  define('OAUTH_URL_ACCESS_TOKEN', 'http://rede.passaporte:3000/oauth/token');
+  define('OAUTH_URL_RESOURCE',     'http://rede.passaporte:3000/oauth/resource');
 
-        // define('MULTISITE', true);
-        // define('SUBDOMAIN_INSTALL', false);
-        // define('DOMAIN_CURRENT_SITE', 'rede.site');
-        // define('PATH_CURRENT_SITE', '/');
-        // define('SITE_ID_CURRENT_SITE', 1);
-        // define('BLOG_ID_CURRENT_SITE', 1);
-
-	}
+  define('MULTISITE', true);
+  define('SUBDOMAIN_INSTALL', true);
+  define('DOMAIN_CURRENT_SITE', 'rede.site');
+  define('PATH_CURRENT_SITE', '/');
+  define('SITE_ID_CURRENT_SITE', 1);
+  define('BLOG_ID_CURRENT_SITE', 1);
 }
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/');
+define('ABSPATH', dirname(__FILE__) . '/');
 
 require(ABSPATH . "../vendor/autoload.php");
 define( 'DB_CONFIG_FILE', ABSPATH . 'db-config.php' );
