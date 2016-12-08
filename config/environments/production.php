e03c00e1aa6ab518efbd800ad7e50af0e5ff1a58
Lucas Pirola <lucaspirola@gmail.com>
2016-12-08 15:58:13 -0200

    set to log errors in file

--- config/environments/production.php
+++ config/environments/production.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', 0);
 define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', false);
+define( 'WP_DEBUG_LOG', true );
+
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
